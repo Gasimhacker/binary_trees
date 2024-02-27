306bf6997d9b17631729a6262eeda8609119cc88
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-27 04:21:26 +0000

    Go  through a binary tree using in-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -100,5 +100,13 @@ int binary_tree_is_root(const binary_tree_t *node);
  * Return: Void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/**
+ * binary_tree_inorder - Go through a binary tree using in-order traversal
+ * @tree: A pointer a pointer to the root node of the tree to traverse
+ * @func: A pointer to a function to call for each node.
+ *
+ * Return: Void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
