a5f2f089fad0ba4173a1e9c79a9474380c3be795
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-28 05:40:30 +0000

    Revert "A node's parent may be Null"
    This reverts commit 6eeb68a974695f145cabab98d36f9706b09d69d1.

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,26 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * recursive_levelorder - Go through a binary tree using
- *			  level-order traversal recursively
- * @tree: A pointer a pointer to the root node of the tree to traverse
- * @func: A pointer to a function to call for each node.
- *
- * Return: Void
- */
-void recursive_levelorder(const binary_tree_t *tree, void (*func)(int))
-{
-	if (!tree || !func)
-		return;
-	if (tree->left)
-		func(tree->left->n);
-	if (tree->right)
-		func(tree->right->n);
-	recursive_levelorder(tree->left, func);
-	recursive_levelorder(tree->right, func);
-}
-
-/**
  * binary_tree_levelorder - Go through a binary tree using
  *			    level-order traversal
  * @tree: A pointer a pointer to the root node of the tree to traverse
@@ -32,6 +12,12 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	func(tree->n);
-	recursive_levelorder(tree, func);
+	if (tree->parent == NULL)
+		func(tree->n);
+	if (tree->left)
+		func(tree->left->n);
+	if (tree->right)
+		func(tree->right->n);
+	binary_tree_levelorder(tree->left, func);
+	binary_tree_levelorder(tree->right, func);
 }
