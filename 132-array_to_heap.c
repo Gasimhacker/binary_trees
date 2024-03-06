f996495f6de1e8cf30bbca6deb456b24f8e9148b
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-06 05:32:26 +0000

    Check if the array is NULL

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap - builds a Max Binary Heap tree from an array
- * @array: a pointer to the first element of the array to be converted
- * @size: the number of element in the array
+ * array_to_heap - Build a Max Binary Heap tree from an array
+ * @array: A pointer to the first element of the array to be converted
+ * @size: The number of element in the array
  *
- * Return: a pointer to the root node of the created Binary Heap
+ * Return: A pointer to the root node of the created Binary Heap
  *         NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
@@ -13,6 +13,8 @@ heap_t *array_to_heap(int *array, size_t size)
 	unsigned int i;
 	heap_t *root = NULL;
 
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		heap_insert(&root, array[i]);
 
--- binary_trees.h
+++ binary_trees.h
@@ -351,5 +351,14 @@ int binary_tree_is_heap(const binary_tree_t *tree);
  *         NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value);
+/**
+ * array_to_heap - Build a Max Binary Heap tree from an array
+ * @array: A pointer to the first element of the array to be converted
+ * @size: The number of element in the array
+ *
+ * Return: A pointer to the root node of the created Binary Heap
+ *         NULL on failure
+ */
+heap_t *array_to_heap(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
